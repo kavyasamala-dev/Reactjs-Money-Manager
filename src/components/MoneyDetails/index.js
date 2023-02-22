8d3ee66f8a444cf657206f16ef5865fdeba25912
Kavya Samala <kavyasamala9@gmail.com>
2023-02-22 12:24:40 +0000

    Commiting Money Manager

---
+++ src/components/MoneyDetails/index.js
@@ -0,0 +1,82 @@
+// Write your code here
+
+const MoneyDetails = props => {
+  const {balanceAmount, incomeAmount, expensesAmount} = props
+  return (
+    <ul>
+      <li>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png"
+          alt="balance"
+        />
+        <p>Your Balance</p>
+        <p data-testid="balanceAmount">Rs {balanceAmount}</p>
+      </li>
+      <li>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png"
+          alt="income"
+        />
+        <p>Your Income</p>
+        <p data-testid="incomeAmount">Rs {incomeAmount}</p>
+      </li>
+      <li>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png"
+          alt="expenses"
+        />
+        <p>Your Expenses</p>
+        <p data-testid="expensesAmount">Rs {expensesAmount}</p>
+      </li>
+    </ul>
+  )
+}
+
+export default MoneyDetails
+
+// import './index.css'
+
+// const MoneyDetails = props => {
+//   const {balanceAmount, incomeAmount, expensesAmount} = props
+//   return (
+//     <ul className="unordered-list-cont">
+//       <li className="list-container bg-color1">
+//         <img
+//           src="https://assets.ccbp.in/frontend/react-js/money-manager/balance-image.png"
+//           alt="balance"
+//           className="image"
+//         />
+//         <div>
+//           <p className="paragraph1">Your Balance</p>
+//           <p className="amount">Rs {balanceAmount}</p>
+//         </div>
+//       </li>
+
+//       <li className="list-container bg-color2">
+//         <img
+//           src="https://assets.ccbp.in/frontend/react-js/money-manager/income-image.png"
+//           alt="income"
+//           className="image"
+//         />
+//         <div>
+//           <p className="paragraph1">Your Income</p>
+//           <p className="amount">Rs {incomeAmount}</p>
+//         </div>
+//       </li>
+
+//       <li className="list-container bg-color3">
+//         <img
+//           src="https://assets.ccbp.in/frontend/react-js/money-manager/expenses-image.png"
+//           alt="expenses"
+//           className="image"
+//         />
+//         <div>
+//           <p className="paragraph1">Your Expenses</p>
+//           <p className="amount">Rs {expensesAmount}</p>
+//         </div>
+//       </li>
+//     </ul>
+//   )
+// }
+
+// export default MoneyDetails
